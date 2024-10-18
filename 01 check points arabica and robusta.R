b07de5cc151ae29862903b294c81ba82e1872803
Castro <F.CASTRO@cgiar.org>
2024-10-18 18:30:13 -0500

    update

--- 01 check points arabica and robusta.R
+++ 01 check points arabica and robusta.R
@@ -1,7 +1,7 @@
 
 # Load libraries ----------------------------------------------------------
 require(pacman)
-pacman::p_load(terra, sf, fs, tidyverse, glue, ggpubr, RColorBrewer, rnaturalearthdata, rnaturalearth)
+pacman::p_load(terra, sf, fs, tidyverse, geodata, glue, rgbif, ggpubr, RColorBrewer, rnaturalearthdata, rnaturalearth)
 
 g <- gc(reset = T)
 rm(list = ls())
@@ -31,8 +31,7 @@ pnts <- as_tibble(cbind(pnts, admn))
 dir.create('./tble')
 write.csv(pnts, './tble/points-zone_ara-rob.csv', row.names = FALSE)
 
-
-# To make the map  --------------------------------------------------------
+# To make the map  --------------------------------------------------------#
 
 gmap <- ggplot() + 
   geom_sf(data = wrld, fill = NA, col = 'grey50') + 
@@ -50,3 +49,62 @@ gmap
 ggsave(plot = gmap, filename = './png/maps/points_ara-rob.jpg', units = 'in', width = 4, height = 5, dpi = 300)
 
 
+# Download GBIF -----------------------------------------------------------
+
+## Points 
+pnts.ourd <- read_csv('./tble/points-zone_ara-rob.csv')
+
+## Points
+pnts.gbif <- occ_search(scientificName = 'Coffea arabica', hasCoordinate = TRUE, hasGeospatialIssue = FALSE)
+pnts.gbif <- pnts.gbif$data
+pnts.gbif <- filter(pnts.gbif, country %in% c('Ethiopia'))
+pnts.gbif <- pnts.gbif %>% dplyr::select(scientificName, decimalLongitude, decimalLatitude)
+
+## Join GBIF and our DB
+pnts.ourd <- pnts.ourd %>% dplyr::select(Species, Longitude, Latitude)
+pnts.gbif <- pnts.gbif %>% dplyr::select(Species = scientificName, Longitude = decimalLongitude, Latitude = decimalLatitude)
+
+pnts.ourd %>% pull(Species) %>% table()
+pnts.gbif %>% pull(Species) %>% table()
+pnts.gbif <- mutate(pnts.gbif, Species = 'Arabica')
+
+pnts.gbif <- pnts.gbif %>% mutate(source = 'GBIF')
+pnts.ourd <- pnts.ourd %>% mutate(source = 'Our DB')
+
+pnts.allp <- rbind(pnts.gbif, pnts.ourd)
+write.csv(pnts.allp, './tble/points-zone_ara-rob_gbif.csv', row.names = FALSE)
+
+
+# Ethiopia ----------------------------------------------------------------
+
+etho <- zone %>% filter(sov_a3 == 'ETH')
+pnts <- rbind(pnts.gbif, pnts.ourd)
+eth1 <- st_as_sf(gadm(country = 'ETH', level = 1, path = './tmpr'))
+lbls <- eth1 %>% st_centroid() %>% st_coordinates() %>% as_tibble() %>% mutate(name = eth1$NAME_1)
+
+pnts <- as_tibble(cbind(pnts, name = terra::extract(vect(etho), pnts[,2:3])[,c('name')]))
+pnts <- filter(pnts, name == 'Ethiopia')
+
+gmap <- ggplot() + 
+  geom_sf(data = wrld, fill = NA, col = 'grey50') + 
+  geom_sf(data = zone, fill = NA, col = 'grey10') + 
+  geom_sf(data = eth1, fill = 'grey80', col = 'grey50') +
+  geom_point(data = pnts, aes(x = Longitude, y = Latitude, col = source), size = 1.2) +
+  scale_color_manual(values = brewer.pal(n = 2, name = 'Set2')) +
+  geom_text_repel(data = lbls, aes(x = X, y = Y, label = name), family = 'Segoe UI', size = 2) +
+  coord_sf(xlim = ext(etho)[1:2], ylim = ext(etho)[3:4]) + 
+  ggtitle(label = 'Coffee arabica points (GBIF + Our database)', 
+          subtitle = ) +
+  labs(x = 'Lon', y = 'Lat', col = 'Coffee') +
+  theme_bw() + 
+  theme(
+    axis.text.y = element_text(angle = 90, hjust = 0.5, size = 6), 
+    axis.text.x = element_text(size = 6), 
+    plot.title = element_text(face = 'bold', hjust = 0.5, size = 14, family = 'Segoe UI'),
+    text = element_text(family = 'Segoe UI'), 
+    legend.position = c(0.9, 0.12)
+  )
+
+ggsave(plot = gmap, filename = './png/maps/points_ara_eth.jpg', units = 'in',width = 7, height = 5, dpi = 300)
+
+write.csv(pnts, './tble/points-eth.csv', row.names = FALSE)
