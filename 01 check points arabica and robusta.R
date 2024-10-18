88b561f9d54cb1e7e1ecbbad97e15ad9ce0dda04
Castro <F.CASTRO@cgiar.org>
2024-10-18 08:51:54 -0500

    update'

--- 01 check points arabica and robusta.R
+++ 01 check points arabica and robusta.R
@@ -1,5 +1,4 @@
 
-
 # Load libraries ----------------------------------------------------------
 require(pacman)
 pacman::p_load(terra, sf, fs, tidyverse, glue, ggpubr, RColorBrewer, rnaturalearthdata, rnaturalearth)
@@ -20,54 +19,34 @@ pnts <- read_csv('D:/OneDrive - CGIAR/Projects/Points/tbl/bd/coffee_v3_2022.csv'
 
 ## Filter for the study zone 
 pnts <- filter(pnts, Country %in% c('Tanzania', 'Uganda', 'Ethiopia'))
+write.csv(pnts, './tble/points-zone_ara-rob.csv', row.names = FALSE)
 sort(unique(pnts$Country))
-
-pnts.ara <- filter(pnts, Species %in% c('Coffee Arabica', 'Coffee arabica', 'Arabica_Otros', 'arabica', 'Arabica'))
-pnts.rob <- filter(pnts, Species %in% c('Coffee Robusta', 'robusta', 'Robusta'))
-
-pnts.ara <- pnts.ara %>% 
-  dplyr::select(Longitude, Latitude) %>% 
-  mutate(name = 'arabica')
-
-pnts.rob <- pnts.rob %>% 
-  dplyr::select(Longitude, Latitude) %>% 
-  mutate(name = 'Robusta')
-
-# Remove duplicated by cell -----------------------------------------------
-
-##
-rmve.dupc <- function(occ){
-  
-  cat('To start\n')
-  msk <- geodata::worldclim_global(var = 'prec', res = 2.5, path = './tmpr')
-  msk <- msk[[1]]
-  cll <- terra::extract(msk, occ[,c('Longitude', 'Latitude')], cell = T)
-  cll <- cll[,3]
-  occ <- mutate(occ, cell = cll)
-  occ <- mutate(occ, dupv = duplicated(cell))
-  occ <- filter(occ, dupv == FALSE)
-  occ <- dplyr::select(occ, name, Longitude, Latitude)
-  return(occ)
-  
-}
-
-##
-pnts.ara <- rmve.dupc(occ = pnts.ara)
-pnts.rob <- rmve.dupc(occ = pnts.rob)
-
-##
-pnts <- rbind(pnts.ara, pnts.rob)
+pnts <- mutate(pnts, Species = ifelse(Species == 'Coffea_arabica', 'Arabica', 'Robusta'))
 
 # To add the country for the points ---------------------------------------
-
 admn <- terra::extract(vect(wrld), pnts[,c('Longitude', 'Latitude')])
-admn <- dplyr::select(admn, name, admin, sov_a3)
-admn <- rename(admn, country = name)
+admn <- dplyr::select(admn, name)
 pnts <- as_tibble(cbind(pnts, admn))
 
 dir.create('./tble')
-write.csv(pnts, './tble/points-world_ara-rob.csv', row.names = FALSE)
+write.csv(pnts, './tble/points-zone_ara-rob.csv', row.names = FALSE)
 
-# Filtering for the study zone --------------------------------------------
-sort(unique(pnts$country))
+
+# To make the map  --------------------------------------------------------
+
+gmap <- ggplot() + 
+  geom_sf(data = wrld, fill = NA, col = 'grey50') + 
+  geom_sf(data = zone, fill = NA, col = 'grey10') + 
+  geom_point(data = pnts, aes(x = Longitude, y = Latitude, col = Species), size = 0.5) +
+  coord_sf(xlim = ext(zone)[1:2], ylim = ext(zone)[3:4]) + 
+  labs(x = 'Lon', y = 'Lat', col = 'Coffee') +
+  theme_bw() + 
+  theme(
+    axis.text.y = element_text(angle = 90, hjust = 0.5, size = 6), 
+    axis.text.x = element_text(size = 6)
+  )
+
+gmap
+ggsave(plot = gmap, filename = './png/maps/points_ara-rob.jpg', units = 'in', width = 4, height = 5, dpi = 300)
+
 
